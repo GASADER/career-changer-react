27fd150c1c9dff38e62f8af05af51db98e79f5ef
GASADER <kajorhkaen@gmail.com>
2023-05-02 10:40:01 +0700

    node ex2

--- 07-mini-project/src/Layout.jsx
+++ 07-mini-project/src/Layout.jsx
@@ -1,6 +1,7 @@
 import UserContext from './UserContext';
 import Navbar from './Navbar';
 
+// set Layout for other child component
 const Layout = ({ children }) => {
     return (
         <div>
--- 08-nodejs/02-exercise/src/main.js
+++ 08-nodejs/02-exercise/src/main.js
@@ -1,4 +1,24 @@
-import express from "express";
+import express from 'express'
+// const express = require('express')
 const webServer = express();
 
 // code down below
+webServer.get('/',(req,res)=>{
+    res.send('<h1>Home</h1>')
+})
+webServer.get('/news',(req,res)=>{
+    res.send('<h1>News</h1>')
+})
+webServer.get('/users',(req,res)=>{
+    res.send('<h1>Users</h1>')
+})
+webServer.get('/me',(req,res)=>{
+    res.send('<h1>Me</h1>')
+})
+webServer.get('/health-check',(req,res)=>{
+    res.send('<h1>health-check</h1>')
+})
+
+webServer.listen(8080,()=>{
+    console.log('Listening port 4000');
+})
